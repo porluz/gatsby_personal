903dbba09f2b9d133d5cff06119db9197a7309ad
Anthony Olivence <anthony.olivence@gmail.com>
2019-09-01 17:29:09 -0400

    Add Styled Component Placeholders

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Button primary>Click me</Button>
+    </>
   );
 }
 export default Home;
